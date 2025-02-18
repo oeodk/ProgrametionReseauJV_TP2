3e3c30f6079156d544de5c6cdee65d710d0d1db5
oeodk <gononlucas2@gmail.com>
2025-02-18 18:46:34 -0500

    POSIX

--- src/falcon_posix.cpp
+++ src/falcon_posix.cpp
@@ -79,7 +79,7 @@ void Falcon::CreateServer(uint16_t port)
     }
     sockaddr local_endpoint = StringToIp("::", port);
     m_socket = socket(local_endpoint.sa_family,
-        SOCK_DGRAM,
+        SOCK_DGRAM | SOCK_NONBLOCK | SOCK_CLOEXEC,
         IPPROTO_UDP);
     if (int error = bind(m_socket, &local_endpoint, sizeof(local_endpoint)); error != 0)
     {
@@ -95,7 +95,7 @@ void Falcon::CreateClient(const std::string& serverIp)
     }
     sockaddr local_endpoint = StringToIp(serverIp, 0);
     m_socket = socket(local_endpoint.sa_family,
-        SOCK_DGRAM,
+        SOCK_DGRAM | SOCK_NONBLOCK | SOCK_CLOEXEC,
         IPPROTO_UDP);
     if (int error = bind(m_socket, &local_endpoint, sizeof(local_endpoint)); error != 0)
     {
@@ -117,16 +117,16 @@ int Falcon::SendToInternal(const std::string &to, uint16_t port, std::span<const
 
 int Falcon::ReceiveFromInternal(std::string &from, std::span<char, 65535> message)
 {
-    struct pollfd fds;
-    fds.fd = m_socket;
-    fds.events = POLLIN;  // Check for data available to read
-
-    int result = poll(&fds, 1, m_timeout_ms);  
-	if (result == 0)
-    {
-        return 0;  // Timeout
-    }
-	
+    //struct pollfd fds;
+    //fds.fd = m_socket;
+    //fds.events = POLLIN;  // Check for data available to read
+	//
+    //int result = poll(&fds, 1, m_timeout_ms);  
+	//if (result == 0)
+    //{
+    //    return 0;  // Timeout
+    //}
+	//
     struct sockaddr_storage peer_addr;
     socklen_t peer_addr_len = sizeof(struct sockaddr_storage);
     const int read_bytes = recvfrom(m_socket,
